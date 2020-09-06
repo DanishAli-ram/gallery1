4721652ab6358fa7c6dca96045b44d0b223c34cf
Danish Ali <danishrao.am@gmail.com>
2020-09-06 19:20:43 +0500

    another commit

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+require('./models/exampleModels');
+
+mongoose.connect(keys.mongoURI);
+
 const app = express();
+
+require('./routes/exampleRoutes')(app);
 
 app.get('/', (req,res) => {
     res.send("Hey there")
